27e354e623910b922e96fe37569c431134c5ade4
James Cuzella <james.cuzella@lyraphase.com>
2014-10-22 16:41:18 -0600

    Adding spec for ::LZOP class constants from lzop-1.03/src/conf.h

---
+++ spec/lzop/lzop_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe ::LZOP do
+  let(:expected_constants) { 
+    {
+      :F_ADLER32_D     => 0x00000001,
+      :F_ADLER32_C     => 0x00000002,
+      :F_STDIN         => 0x00000004,
+      :F_STDOUT        => 0x00000008,
+      :F_NAME_DEFAULT  => 0x00000010,
+      :F_DOSISH        => 0x00000020,
+      :F_H_EXTRA_FIELD => 0x00000040,
+      :F_H_GMTDIFF     => 0x00000080,
+      :F_CRC32_D       => 0x00000100,
+      :F_CRC32_C       => 0x00000200,
+      :F_MULTIPART     => 0x00000400,
+      :F_H_FILTER      => 0x00000800,
+      :F_H_CRC32       => 0x00001000, # This is used currently
+      :F_H_PATH        => 0x00002000,
+      :F_MASK          => 0x00003FFF,
+
+      :ADLER32_INIT_VALUE => 1, # This is used currently
+      :CRC32_INIT_VALUE   => 0  # This is used currently
+    }
+  }
+  
+  it "should contain constants from lzop/conf.h" do
+    expected_constants.each do |constant, value|
+      expect(subject.constants).to include(constant)
+      expect(subject.const_get(constant)). to eq value
+    end
+  end
+end
