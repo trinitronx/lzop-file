bcbbaf87b2193aede7a1ebc971c983ec8dedc5a2
James Cuzella <james.cuzella@lyraphase.com>
2014-10-27 17:21:28 -0600

    Refactor RSpec tests
     - Use global vars for test fixture constants
     - Use before(:all/:context) for test file creation
     - Test header fields individually
     - Add 'pending' spec placeholder for optional header field: 'filter'
     - Add test coverage for all LZOP file header fields up to and excluding mtime_low (and filter)

--- spec/lzop/file_spec.rb
+++ spec/lzop/file_spec.rb
@@ -1,6 +1,11 @@
 
 require 'spec_helper'
 
+## TODO: put this in helpers if needed?
+def bin_to_hex(s)
+    s.each_byte.map { |b| b.to_s(16).rjust(2,'0') }.join
+end
+
 ## Test internal class header fields & magic bits
 describe LZOP::File::Header.members do
   
@@ -14,26 +19,80 @@ describe LZOP::File::Header.members do
 end
 
 describe 'LZOP::File' do
-  # These should never change, so hardcode away...
-  let(:expected_lzop_magic) { [ 0x89, 0x4c, 0x5a, 0x4f, 0x00, 0x0d, 0x0a, 0x1a, 0x0a ] }
-  let(:uncompressed_file_data) { "Hello World\n" * 100 }
-  let(:filename) { 'lzoptest.lzo' }
-  let(:test_fixture_path) { File.join(File.dirname(__FILE__), '..', 'fixtures', filename) }
-  let(:lzop_test_fixture_file_data) { File.open( test_fixture_path, 'rb').read }
-  let(:tmp_filename) { File.basename(filename, File.extname(filename) ) }
-  let(:tmp_file_path) { File.join( '', 'tmp', tmp_filename) }
+  before(:all) {
+    @expected_lzop_magic = [ 0x89, 0x4c, 0x5a, 0x4f, 0x00, 0x0d, 0x0a, 0x1a, 0x0a ]
+    @uncompressed_file_data = "Hello World\n" * 100
+    @filename = 'lzoptest.lzo'
+    @test_fixture_path = File.join(File.dirname(__FILE__), '..', 'fixtures', @filename + '.3')
+    @lzop_test_fixture_file_data = File.open( @test_fixture_path, 'rb').read
+    @tmp_filename = File.basename(@filename)
+    @tmp_file_path = File.join( '', 'tmp', @tmp_filename)
+  }
 
-  subject { LZOP::File }
-
-  it 'has correct lzop_magic bits' do
-    expect(subject.class_variable_get(:@@lzop_magic)).to eq expected_lzop_magic
+  it 'uses correct lzop_magic bits' do
+    expect(LZOP::File.class_variable_get(:@@lzop_magic)).to eq @expected_lzop_magic
   end
 
-  it 'writes a correct LZO file header' do
-    my_test_file = subject.new( tmp_file_path )
-    my_test_file.write( uncompressed_file_data )
-    test_file_data = File.open( tmp_file_path, 'rb').read
+  context 'when given a filename, no options and writing uncompressed test data' do
+    before(:all) {
+      my_test_file = LZOP::File.new( @tmp_file_path )
+      my_test_file.write( @uncompressed_file_data )
+      @test_file_data = File.open( @tmp_file_path, 'rb').read
+    }
 
-    expect(test_file_data).to eq lzop_test_fixture_file_data
+    describe 'the output binary file' do
+      it 'has the correct magic bits' do
+        expect( @test_file_data[0..8].unpack('C*') ).to eq @expected_lzop_magic
+      end
+
+      it 'has a correct version in LZO file header' do
+        expect(@test_file_data[9..10]).to eq @lzop_test_fixture_file_data[9..10]
+        # expect(test_file_data).to eq lzop_test_fixture_file_data
+        # expect(Digest::MD5.hexdigest(test_file_data)).to eq Digest::MD5.hexdigest(lzop_test_fixture_file_data)
+      end
+
+      it 'has the correct library version in LZO file header' do
+        expected_library_version = LZO::LZO_VERSION.is_a?(Numeric) ? LZO::LZO_VERSION : 0x2080
+        expect(@test_file_data[11..12]).to eq [expected_library_version].pack('S>')
+      end
+
+      it 'has the correct lib version needed to extract in LZO file header' do
+        expect(@test_file_data[13..14]).to eq [ 0x0940 ].pack('S>')
+      end
+
+      it 'has the correct method of compression in LZO file header' do
+        expect(@test_file_data[15]).to eq @lzop_test_fixture_file_data[15]
+      end
+
+      it 'has the correct level of compression in LZO file header' do
+        expect(@test_file_data[16]).to eq @lzop_test_fixture_file_data[16]
+      end
+
+      it 'has the compression flags in LZO file header' do
+        expect(@test_file_data[17..21]).to eq @lzop_test_fixture_file_data[17..21]
+      end
+
+      ## Optional field :filter seems to be set if :flags has F_H_FILTER bit set
+      ## LZOP docs say filters are "Rarely useful" so we do not support this!
+      it 'has the filter in LZO file header' do
+        unless @test_file_data[17..21].unpack('L>').first & LZOP::F_H_FILTER == 0
+          pending('Optional header: Filter is not supported by lzop-file')
+          fail
+          # expect(@test_file_data[22..25]).to eq 'SOMETHING'
+        end
+      end
+
+      it 'has the original file mode in LZO file header' do
+        if @test_file_data[17..21].unpack('L>').first & LZOP::F_H_FILTER == 0
+          start_byte=22
+          end_byte=25
+        else
+          start_byte=26
+          end_byte=29
+        end
+
+        expect(@test_file_data[start_byte..end_byte]).to eq @lzop_test_fixture_file_data[start_byte..end_byte]
+      end
+    end
   end
 end
